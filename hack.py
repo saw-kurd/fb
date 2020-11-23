7b711350de617d94f1d02de3b2ed73a88189f76b
Ara Software <71103842+cr4ck3r1@users.noreply.github.com>
2020-11-23 14:30:24 +0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,24 +1,2 @@
-import uuid
-import requests
-import time
-import os
-
-c = input("nawt chia? ")
-    
-e = str(uuid.uuid5(uuid.NAMESPACE_DNS, c))
-print("")
-print(e)
-g = requests.get("https://pastebin.com/2WuEPjdc")
-
-if e in g.text:
-    print("active")
-    time.sleep(2)
-    from lazytool import hack
-    hack()
-    
-else:
-    print("id'akat active nia ")
-    print("")
-    print("nama bo instakam bnera")
-    print("")
-    print(" instagram: ara_software")
+from lazytool import hack
+hack()
