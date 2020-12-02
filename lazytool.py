997ca79e849059fb07e9863cc3f9d8fd4b033ed2
Ara Software <71103842+cr4ck3r1@users.noreply.github.com>
2020-12-02 22:34:17 +0300

    Update lazytool.py

--- lazytool.py
+++ lazytool.py
@@ -608,26 +608,3 @@ def hack():
     if __name__ == '__main__':
         login()
         
-
-os.system("clear")
-
-c = input("nawt chia? ")
-    
-e = str(uuid.uuid5(uuid.NAMESPACE_DNS, c))
-print("")
-print(e)
-g = requests.get("https://pastebin.com/2WuEPjdc")
-
-if e in g.text:
-    print("active")
-    time.sleep(2)
-    hack()
-    
-else:
-    print("id'akat active nia ")
-    print("")
-    print("nama bo instakam bnera")
-    print("")
-    print(" instagram: ara_software")
-    
-
