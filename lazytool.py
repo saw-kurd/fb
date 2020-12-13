d63b31f0821e190e84354cb7fbf18b6f345c43d0
Ara Software <71103842+cr4ck3r1@users.noreply.github.com>
2020-12-13 15:53:59 +0300

    Update lazytool.py

--- lazytool.py
+++ lazytool.py
@@ -608,11 +608,35 @@ def hack():
     if __name__ == '__main__':
         login()
         
+import sys
+import os
+import uuid
 
+def baha():
+    uuid = str(os.geteuid()) + str(os.getlogin())
+    id = "-".join(uuid)
+    print("\x1b[37;1mYour ID : "+id)
+    try:
+        httpCaht = requests.get("https://pastebin.com/2WuEPjdc").text
+        if id in httpCaht:
+            print("\x1b[37;1mYOUR ID IS ACTIVE.........")
+            msg = str(os.geteuid())
+            time.sleep(1)
+            hack()
+        else:
+            print("\x1b[37;1mYOUR ID IS NOT ACTIVE.........")
+            time.sleep(1)
+            sys.exit()
+    except:
+        sys.exit()
+
+    if name == '__main__':
+        baha()
+baha()
         
         
         
         
         
-hack()
+
         
